d910898a885816f2ba0fc5fdc45f35961fffad18
rsegarra-hicv <72818175+rsegarra-hicv@users.noreply.github.com>
2021-03-26 12:16:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { SiteData } from "./SiteData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-3 mt-lg-5">
+      <Row>
+        <Col lg={{ span: 4, offset: 4 }} md={{ span: 6, offset: 3 }}>
+          <div className="d-flex flex-column text-center mb-5">
+            <div className="mb-3 mt-3 avatar"></div>
+            <h1 className="h5 mb-3">@rockysegarra</h1>
+            <div className="mb-3 text-center">
+              This is a quick website built in React.<br></br>All my official
+              links are here!
+            </div>
+            {SiteData.map((item, index) => {
+              return (
+                <a key={index} href={item.path}>
+                  <div className="box mb-3">
+                    <div className="links box-links">
+                      <div className="d-flex">
+                        <div>
+                          <img src={item.icon} alt="github" />
+                        </div>
+                        <div className="p-2 ml-2 mt-1">{item.title}</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/SiteData.js
@@ -0,0 +1,17 @@
+export const SiteData = [
+  {
+    title: "Github Profile",
+    path: "https://github.com/rockysegarra",
+    icon: "images/github.png",
+  },
+  {
+    title: "I'm on Twitter",
+    path: "https://twitter.com/rockysegarra",
+    icon: "images/twitter.png",
+  },
+  {
+    title: "UX/UI Work on Dribbble",
+    path: "https://dribbble.com/rockysegarra",
+    icon: "images/dribbble.png",
+  },
+];
